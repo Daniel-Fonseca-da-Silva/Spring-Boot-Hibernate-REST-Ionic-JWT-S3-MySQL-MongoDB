7f75fc6ee5937b28d50f06886469bafd20b61c3c
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-02-18 23:23:55 -0300

    Criando operação de instânciação

--- src/main/java/com/curso/mc/CursomcApplication.java
+++ src/main/java/com/curso/mc/CursomcApplication.java
@@ -1,13 +1,40 @@
 package com.curso.mc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.curso.mc.domain.Categoria;
+import com.curso.mc.repositories.CategoriaRepository;
 
-	public static void main(String[] args) {
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+	
+	@Autowired
+	CategoriaRepository categoriaRepository;
+
+	public static void main(String[] args) 
+	{
 		SpringApplication.run(CursomcApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception 
+	{
+		
+		// Cria objetos
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		
+		// Salva no BD
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+	}
+	
+	
 
 }
